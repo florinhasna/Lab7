e538a5d4cd04442db87b59efce6a3e20d735e428
Florin Hasna <florinhasna@outlook.com>
2023-12-08 10:44:36 +0000

    User enter details for all employees in array, program print name and ID of all

--- arrayOfEmployees.cpp
+++ arrayOfEmployees.cpp
@@ -5,7 +5,7 @@ int main(){
     struct employee{
     string first_name;
     string last_name;
-    int ID;
+    string ID;
     string email;
     string phone_number;
     string salary;
@@ -13,8 +13,33 @@ int main(){
   };
 
   int numberOfEmployees;
-  cout << "Enter how many employees you want to add: " << endl;
+  cout << "Enter how many employees you want to add: ";
   cin >> numberOfEmployees;
 
   employee* staffDB = new employee[numberOfEmployees];
+  cout << "\n";
+  for(int i = 0; i < numberOfEmployees; i++){
+    cout << "Enter employee number " << i + 1 << endl;
+    cout << "Enter first name: ";
+    cin >> staffDB[i].first_name;
+    cout << "Enter last name: ";
+    cin >> staffDB[i].last_name;
+    cout << "Enter ID: ";
+    cin >> staffDB[i].ID;
+    cout << "Enter email: ";
+    cin >> staffDB[i].email;
+    cout << "Enter phone number: ";
+    cin >> staffDB[i].phone_number;
+    cout << "Enter salary: ";
+    cin >> staffDB[i].salary;
+    cout << "Enter manager: ";
+    cin >> staffDB[i].manager;
+    cout << "\n";
+  }
+  
+  for(int i = 0; i < numberOfEmployees; i++){
+    cout << staffDB[i].first_name << " " << staffDB[i].last_name << "\t" << staffDB[i].ID << endl;
+  }
+
+  return 0;
 }
